fbddb28e085d91611547875400bd4aa851d1c17e
alex <sytnikov.zp@gmail.com>
2024-01-29 00:33:55 +0200

    Enter in console not simple nubmers

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@
 
 let num = 10; // set interval
 
-nextPrime: for (let i = 1; i <= num; i++) {
-  if (num % i === 0) {
+nextIteration:
+for (let i = 1; i <= num; i++) { // For i to *num*
+  for (let j = 2; j < i; j++) { // check remainder of division
+    if (i % j === 0) console.log(i+' is not Prime');
   }
-  console.log(num +'%'+ i +'='+ num % i);
+
+  // console.log(i); 
 }
